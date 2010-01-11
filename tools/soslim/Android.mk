209405fff845c2b859bea82a25a436ea1f936cf7
Chris Dearman <chris@mips.com>
2010-01-11 14:52:24 -0800

    [MIPS] Initial support for MIPS compilation environment
    Signed-off-by: Chris Dearman <chris@mips.com>

--- tools/soslim/Android.mk
+++ tools/soslim/Android.mk
@@ -5,7 +5,7 @@
 
 LOCAL_PATH:= $(call my-dir)
 
-ifeq ($(TARGET_ARCH),arm)
+ifneq ($(TARGET_ARCH),x86)
 include $(CLEAR_VARS)
 
 LOCAL_LDLIBS += -ldl
@@ -13,11 +13,19 @@ LOCAL_CFLAGS += -O2 -g
 LOCAL_CFLAGS += -fno-function-sections -fno-data-sections -fno-inline
 LOCAL_CFLAGS += -Wall -Wno-unused-function #-Werror
 LOCAL_CFLAGS += -DBIG_ENDIAN=1
-LOCAL_CFLAGS += -DARM_SPECIFIC_HACKS
 LOCAL_CFLAGS += -DSUPPORT_ANDROID_PRELINK_TAGS
 LOCAL_CFLAGS += -DDEBUG
 LOCAL_CFLAGS += -DSTRIP_STATIC_SYMBOLS
 LOCAL_CFLAGS += -DMOVE_SECTIONS_IN_RANGES
+LOCAL_CFLAGS += -DTARGET_ARCH_$(TARGET_ARCH)
+
+ifeq ($(TARGET_ARCH),arm)
+LOCAL_CFLAGS += -DARM_SPECIFIC_HACKS
+endif
+
+ifeq ($(TARGET_ARCH),mips)
+LOCAL_CFLAGS += -DMIPS_SPECIFIC_HACKS
+endif
 
 ifeq ($(HOST_OS),windows)
 # Cygwin stat does not support ACCESSPERMS bitmask
@@ -41,9 +49,12 @@ LOCAL_C_INCLUDES:= \
 	external/elfutils/libebl/ \
 	external/elfcopy/
 
-LOCAL_STATIC_LIBRARIES := libelfcopy libelf libebl libebl_arm #dl
+LOCAL_STATIC_LIBRARIES := libelfcopy libelf libebl #dl
+ifeq ($(TARGET_ARCH),arm)
+LOCAL_STATIC_LIBRARIES += libebl_arm
+endif
 
 LOCAL_MODULE := soslim
 
 include $(BUILD_HOST_EXECUTABLE)
-endif #TARGET_ARCH==arm
+endif #TARGET_ARCH!=x86
