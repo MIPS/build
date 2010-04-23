882b687a9ef783bbae5bc32dccf51c312331af00
Nikola Kuzmanovic <nikola.kuzmanovic@rt-rk.com>
2010-04-22 18:56:37 -0700

    [ENDIAN] - Fix the byteorder of keycharmap generator

--- tools/kcm/Android.mk
+++ tools/kcm/Android.mk
@@ -8,6 +8,13 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := \
 	kcm.cpp
 
+ifeq ($(TARGET_ARCH),mips)
+LOCAL_CFLAGS += -DMIPS_SPECIFIC_HACKS
+ifeq ($(TARGET_CPU_ENDIAN),EB)
+LOCAL_CFLAGS += -DBYTE_ORDER_BIG_ENDIAN=1
+endif
+endif
+
 LOCAL_MODULE := kcm
 
 include $(BUILD_HOST_EXECUTABLE)
