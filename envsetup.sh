08e5f9b05edba26a40ad75d9b38a1186d694a417
Chris Dearman <chris@mips.com>
2010-09-30 20:55:46 -0700

    Display architecture for tapas builds
    Change-Id: If7102463e9d646a7cd80186ca56f07361d668c2d

--- envsetup.sh
+++ envsetup.sh
@@ -186,7 +186,7 @@ function settitle()
         if [ -z "$apps" ]; then
             export PROMPT_COMMAND="echo -ne \"\033]0;[${product}-${variant}] ${USER}@${HOSTNAME}: ${PWD}\007\""
         else
-            export PROMPT_COMMAND="echo -ne \"\033]0;[$apps $variant] ${USER}@${HOSTNAME}: ${PWD}\007\""
+            export PROMPT_COMMAND="echo -ne \"\033]0;[$arch $apps $variant] ${USER}@${HOSTNAME}: ${PWD}\007\""
         fi
     fi
 }
