15a672eb280f885fe1b63d26fab0a465e02c42a7
Raghu Gandham <raghu@mips.com>
2011-05-09 15:00:08 -0700

    Use compiler 4.4.6. The newer binutils carry fixes for linker issues caused when compiling newer versions of webkit and v8.
    (cherry picked from commit 2eadaacc0373197432ca8b6607e2b1baad4615bb)
    
    Change-Id: Ib27b4696c2478e12971061e427e3b5acf0de9147

--- core/combo/TARGET_linux-mips.mk
+++ core/combo/TARGET_linux-mips.mk
@@ -44,7 +44,7 @@ include $(TARGET_ARCH_SPECIFIC_MAKEFILE)
 # You can set TARGET_TOOLS_PREFIX to get gcc from somewhere else
 ifeq ($(strip $(TARGET_TOOLS_PREFIX)),)
 TARGET_TOOLS_PREFIX := \
-	prebuilt/$(HOST_PREBUILT_TAG)/toolchain/mips-4.4.3/bin/mips-linux-gnu-
+	prebuilt/$(HOST_PREBUILT_TAG)/toolchain/mips-4.4.6/bin/mips-linux-gnu-
 endif
 
 TARGET_CC := $(TARGET_TOOLS_PREFIX)gcc$(HOST_EXECUTABLE_SUFFIX)
