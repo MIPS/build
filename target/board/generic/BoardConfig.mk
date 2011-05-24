95945043f8126b67039c00087c8d2323d0e4e20d
Bhanu Chetlapalli <bhanu@mips.com>
2011-05-24 15:49:21 -0700

    [MIPS] Add support for new ABI strings for compatibility with NDK Apps
    Change-Id: I9b0598d797dba27876f474a83475b7b642d836ad
    Signed-Off-By: Bhanu Chetlapalli <bhanu@mips.com>

--- target/board/generic/BoardConfig.mk
+++ target/board/generic/BoardConfig.mk
@@ -11,7 +11,48 @@ ifeq ($(TARGET_ARCH),arm)
 TARGET_CPU_ABI := armeabi
 endif
 ifeq ($(TARGET_ARCH),mips)
-TARGET_CPU_ABI := mipso32
+ifeq (,$(findstring -eb,$(TARGET_ARCH_VARIANT)))
+
+ifneq (,$(findstring -fp,$(TARGET_ARCH_VARIANT)))
+# Hard Float LE
+ifeq (,$(findstring r2,$(TARGET_ARCH_VARIANT)))
+TARGET_CPU_ABI  := mips
+else
+TARGET_CPU_ABI  := mips-r2
+TARGET_CPU_ABI2 := mips
 endif
+else
+# Soft Float LE
+ifeq (,$(findstring r2,$(TARGET_ARCH_VARIANT)))
+TARGET_CPU_ABI  := mips-sf
+else
+TARGET_CPU_ABI  := mips-r2-sf
+TARGET_CPU_ABI2 := mips-sf
+endif
+endif
+
+else # BE systems
+
+ifneq (,$(findstring -fp,$(TARGET_ARCH_VARIANT)))
+# Hard Float BE
+ifeq (,$(findstring r2,$(TARGET_ARCH_VARIANT)))
+TARGET_CPU_ABI  := mips-eb
+else
+TARGET_CPU_ABI  := mips-eb-r2
+TARGET_CPU_ABI2 := mips-eb
+endif
+else
+# Soft Float BE
+ifeq (,$(findstring r2,$(TARGET_ARCH_VARIANT)))
+TARGET_CPU_ABI  := mips-eb-sf
+else
+TARGET_CPU_ABI  := mips-eb-r2-sf
+TARGET_CPU_ABI2 := mips-eb-sf
+endif
+endif
+
+endif
+endif
+
 HAVE_HTC_AUDIO_DRIVER := true
 BOARD_USES_GENERIC_AUDIO := true
