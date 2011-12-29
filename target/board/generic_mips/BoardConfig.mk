e616547b27cd65334d64c4a202ee166f408e92ce
Chris Dearman <chris@mips.com>
2011-12-28 16:02:02 -0800

    [MERGE] Update full_mips target to match default full target
    Change-Id: I84df3c3163e04a28663864159e59b012114491f7
    Signed-off-by: Chris Dearman <chris@mips.com>

--- target/board/generic_mips/BoardConfig.mk
+++ target/board/generic_mips/BoardConfig.mk
@@ -60,7 +60,20 @@ BOARD_USES_GENERIC_AUDIO := true
 # no hardware camera
 USE_CAMERA_STUB := true
 
-# Set /system/bin/sh to mksh, not ash, to test the transition.
-TARGET_SHELL := mksh
+# Set /system/bin/sh to ash, not mksh, to make sure we can switch back.
+TARGET_SHELL := ash
 
+# Enable dex-preoptimization to speed up the first boot sequence
+# of an SDK AVD. Note that this operation only works on Linux for now
+ifeq ($(HOST_OS),linux)
+  ifeq ($(WITH_DEXPREOPT),)
+    WITH_DEXPREOPT := true
+  endif
+endif
 
+# Build OpenGLES emulation guest and host libraries
+BUILD_EMULATOR_OPENGL := true
+
+# Build and enable the OpenGL ES View renderer. When running on the emulator,
+# the GLES renderer disables itself if host GL acceleration isn't available.
+USE_OPENGL_RENDERER := true
