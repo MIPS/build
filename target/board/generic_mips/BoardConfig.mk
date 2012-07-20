8df25e450c5539d2436064216a64b331fd0ae360
Duane Sand <duanes@mips.com>
2012-07-20 12:32:32 -0700

    [MIPS] Reduce mips-r2 etc abi names to just mips, for SDK purposes.
    Adjust generic_mips/BoardConfig.mk to match pre-JB aosp master branch,
    including changes from Raghu's May 7 "MIPS generic target support" and
    typo correction from Raghu's May 31 "The patch addresses the below issues".
    
    SDK Manager rejects system image files that say ro.product.cpu.abi=mips-r2
    or SystemImage.Abi=mips-r2.
    
    Change-Id: I60052d594932eafb187dc0a2c760d3886c64c6e0

--- target/board/generic_mips/BoardConfig.mk
+++ target/board/generic_mips/BoardConfig.mk
@@ -1,3 +1,18 @@
+#
+# Copyright (C) 2011 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 # BoardConfig.mk
 #
 # Product-specific compile-time definitions.
@@ -11,48 +26,7 @@ TARGET_ARCH := mips
 ifeq (,$(TARGET_ARCH_VARIANT))
 TARGET_ARCH_VARIANT := mips32r2-fp
 endif
-
-ifeq (,$(findstring -eb,$(TARGET_ARCH_VARIANT)))
-
-ifneq (,$(findstring -fp,$(TARGET_ARCH_VARIANT)))
-# Hard Float LE
-ifeq (,$(findstring r2,$(TARGET_ARCH_VARIANT)))
 TARGET_CPU_ABI  := mips
-else
-TARGET_CPU_ABI  := mips-r2
-TARGET_CPU_ABI2 := mips
-endif
-else
-# Soft Float LE
-ifeq (,$(findstring r2,$(TARGET_ARCH_VARIANT)))
-TARGET_CPU_ABI  := mips-sf
-else
-TARGET_CPU_ABI  := mips-r2-sf
-TARGET_CPU_ABI2 := mips-sf
-endif
-endif
-
-else # BE systems
-
-ifneq (,$(findstring -fp,$(TARGET_ARCH_VARIANT)))
-# Hard Float BE
-ifeq (,$(findstring r2,$(TARGET_ARCH_VARIANT)))
-TARGET_CPU_ABI  := mips-eb
-else
-TARGET_CPU_ABI  := mips-eb-r2
-TARGET_CPU_ABI2 := mips-eb
-endif
-else
-# Soft Float BE
-ifeq (,$(findstring r2,$(TARGET_ARCH_VARIANT)))
-TARGET_CPU_ABI  := mips-eb-sf
-else
-TARGET_CPU_ABI  := mips-eb-r2-sf
-TARGET_CPU_ABI2 := mips-eb-sf
-endif
-endif
-
-endif
 
 HAVE_HTC_AUDIO_DRIVER := true
 BOARD_USES_GENERIC_AUDIO := true
